a9f47937e91515800acd30f461ed43a2e574f731
srikantht <srikantht@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-04-30 10:11:27 +0000

    Implemented get bar list web service.
    git-svn-id: http://192.168.2.90/svn/Vendsy/Bartsy/Android/Bartsy@16 c3b44e55-a100-42f6-9c86-32dbf579054b

---
+++ src/com/kellislabs/bartsy/utils/WebServices.java
@@ -0,0 +1,79 @@
+package com.kellislabs.bartsy.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+public class WebServices {
+
+	// checking internet connection
+	public static boolean isNetworkAvailable(Context context) throws Exception {
+
+		ConnectivityManager cm = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (cm.getActiveNetworkInfo() != null
+				&& cm.getActiveNetworkInfo().isFailover())
+			return false;
+		else if (cm.getActiveNetworkInfo() != null
+
+		&& cm.getActiveNetworkInfo().isAvailable()
+				&& cm.getActiveNetworkInfo().isConnected()) {
+
+			return true;
+
+		}
+
+		else {
+
+			return false;
+		}
+
+	}
+
+	/*
+	 * This Method i am using for each and every request which is going through
+	 * get() method.
+	 */
+	public static String getBarList(String url, Context context) {
+
+		BufferedReader bufferReader = null;
+		StringBuffer stringBuffer = new StringBuffer("");
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpGet httpRequest = new HttpGet();
+		String result = "";
+
+		try {
+			boolean status = isNetworkAvailable(context);
+			if (status == true) {
+
+				httpRequest.setURI(new URI(url));
+
+				HttpResponse response = httpClient.execute(httpRequest);
+
+				bufferReader = new BufferedReader(new InputStreamReader(response
+						.getEntity().getContent()));
+				String line = "";
+				String NL = System.getProperty("line.separator");
+				while ((line = bufferReader.readLine()) != null) {
+					stringBuffer.append(line + NL);
+				}
+				bufferReader.close();
+
+			}
+			result = stringBuffer.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+}
