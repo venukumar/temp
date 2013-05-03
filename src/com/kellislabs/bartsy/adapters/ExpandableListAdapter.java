8236a16735c3397d5543640af67aefed16b392d8
malireddy <malireddy@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-03 08:45:43 +0000

    Fixed the child view in Expandable list view -  menu items
    git-svn-id: http://192.168.2.90/svn/Vendsy/Bartsy/Android/Bartsy@49 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/kellislabs/bartsy/adapters/ExpandableListAdapter.java
+++ src/com/kellislabs/bartsy/adapters/ExpandableListAdapter.java
@@ -7,14 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
-import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.kellislabs.bartsy.R;
 import com.kellislabs.bartsy.model.MenuDrink;
-
-
-
 
 public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
@@ -45,14 +41,14 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         if( convertView != null )
             v = convertView;
         else
-            v = inflater.inflate(R.layout.child_row, parent, false); 
+            v = inflater.inflate(R.layout.drink_item, parent, false); 
         MenuDrink c = (MenuDrink)getChild( groupPosition, childPosition );
-		TextView color = (TextView)v.findViewById( R.id.childname );
-		if( color != null )
-			color.setText( c.getTitle() );
-		TextView rgb = (TextView)v.findViewById( R.id.rgb );
+		TextView textView = (TextView)v.findViewById( R.id.view_drink_title );
+		if( textView != null )
+			textView.setText( c.getTitle() );
+		TextView rgb = (TextView)v.findViewById( R.id.view_drink_price );
 		if( rgb != null )
-			rgb.setText( c.getPrice() );
+			rgb.setText( "$"+c.getPrice() );
 		
         return v;
     }
@@ -78,9 +74,9 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
         if( convertView != null )
             v = convertView;
         else
-            v = inflater.inflate(R.layout.group_row, parent, false); 
+            v = inflater.inflate(R.layout.list_item_parent, parent, false); 
         String gt = (String)getGroup( groupPosition );
-		TextView colorGroup = (TextView)v.findViewById( R.id.childname );
+		TextView colorGroup = (TextView)v.findViewById( R.id.list_item_text_view);
 		if( gt != null )
 			colorGroup.setText( gt );
         return v;
@@ -96,168 +92,4 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
     public void onGroupCollapsed (int groupPosition) {} 
     public void onGroupExpanded(int groupPosition) {}
-
-
 }
-
-
-//public class ExpandableListAdapter extends BaseExpandableListAdapter {
-//
-//	private Context mContext;
-//	private ExpandableListView mExpandableListView;
-//	private List<Section> mGroupCollection;
-//	private int[] groupStatus;
-//	LinearLayout layout;
-//
-//
-//	public ExpandableListAdapter(Context pContext,
-//			ExpandableListView pExpandableListView,
-//			List<Section> pGroupCollection) {
-//		mContext = pContext;
-//		mGroupCollection = pGroupCollection;
-//		mExpandableListView = pExpandableListView;
-//		groupStatus = new int[mGroupCollection.size()];
-//
-//		setListEvent();
-//	}
-//
-//	private void setListEvent() {
-//
-//		mExpandableListView
-//				.setOnGroupExpandListener(new OnGroupExpandListener() {
-//
-//					@Override
-//					public void onGroupExpand(int arg0) {
-//
-//						groupStatus[arg0] = 1;
-//
-//					}
-//				});
-//
-//		mExpandableListView
-//				.setOnGroupCollapseListener(new OnGroupCollapseListener() {
-//
-//					@Override
-//					public void onGroupCollapse(int arg0) {
-//
-//						groupStatus[arg0] = 0;
-//					}
-//				});
-//	}
-//
-//	@Override
-//	public String getChild(int arg0, int arg1) {
-//
-//		return mGroupCollection.get(arg0).getDrinks().get(arg1).getDescription();
-//	}
-//
-//	@Override
-//	public long getChildId(int arg0, int arg1) {
-//
-//		return 0;
-//	}
-//
-//	@Override
-//	public View getChildView(int arg0, int arg1, boolean arg2, View arg3,
-//			ViewGroup arg4) {
-//
-//		ChildHolder childHolder;
-//		if (arg3 == null) {
-//			arg3 = LayoutInflater.from(mContext).inflate(
-//					R.layout.list_item_child, null);
-//
-//			childHolder = new ChildHolder();
-//
-//			childHolder.title = (TextView) arg3.findViewById(R.id.list_item_text_child);
-//			arg3.setTag(childHolder);
-//		} else {
-//			childHolder = (ChildHolder) arg3.getTag();
-//		}
-//
-//		childHolder.title
-//				.setText(mGroupCollection.get(arg0).getDrinks()
-//						.get(arg1).getTitle());
-//		return arg3;
-//
-//	}
-//
-//	@Override
-//	public int getChildrenCount(int arg0) {
-//
-//		return mGroupCollection.get(arg0).getDrinks().size();
-//	}
-//
-//	@Override
-//	public Object getGroup(int arg0) {
-//
-//		return mGroupCollection.get(arg0);
-//	}
-//
-//	@Override
-//	public int getGroupCount() {
-//		int i = mGroupCollection.size();
-//		return i;
-//	}
-//
-//	@Override
-//	public long getGroupId(int arg0) {
-//
-//		return arg0;
-//	}
-//
-//	@Override
-//	public View getGroupView(int arg0, boolean arg1, View arg2, ViewGroup arg3) {
-//
-//		GroupHolder groupHolder;
-//		if (arg2 == null) {
-//
-//			
-//			arg2 = LayoutInflater.from(mContext).inflate(R.layout.list_item_parent,
-//					null);
-//			groupHolder = new GroupHolder();
-//			groupHolder.title = (TextView) arg2.findViewById(R.id.list_item_text_view);
-//
-//			arg2.setTag(groupHolder);
-//		} else {
-//			groupHolder = (GroupHolder) arg2.getTag();
-//		}
-////		groupHolder.image.setImageResource(imgid[arg0]);
-//		groupHolder.title.setTypeface(null, Typeface.BOLD);
-//		if (groupStatus[arg0] == 0) {
-////			groupHolder.img.setImageResource(R.drawable.arrowexpand);
-//			groupHolder.title.setTextColor(Color.BLACK);
-//		} else {
-////			groupHolder.img.setImageResource(R.drawable.arrowexpanded);
-//			groupHolder.title.setTextColor(Color.GRAY);
-//		}
-//
-//		groupHolder.title.setText(mGroupCollection.get(arg0).getName());
-//	
-//
-//		return arg2;
-//	}
-//
-//	class GroupHolder {
-//		ImageView img;
-//		TextView title;
-//		ImageView image;
-//
-//	}
-//
-//	class ChildHolder {
-//		TextView title;
-//	}
-//
-//	@Override
-//	public boolean hasStableIds() {
-//
-//		return true;
-//	}
-//
-//	@Override
-//	public boolean isChildSelectable(int arg0, int arg1) {
-//
-//		return true;
-//	}
-//
-//}
