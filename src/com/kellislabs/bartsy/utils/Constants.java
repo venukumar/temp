4cc05e1b94d86a3cde266cd910e18133367aeef8
malireddy <malireddy@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-06 14:41:35 +0000

    Changed to post method web services and the server URL
    git-svn-id: http://192.168.2.90/svn/Vendsy/Bartsy/Android/Bartsy@76 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/kellislabs/bartsy/utils/Constants.java
+++ src/com/kellislabs/bartsy/utils/Constants.java
@@ -2,10 +2,14 @@ package com.kellislabs.bartsy.utils;
 
 public class Constants {
 
-	public static final String DOMAIN_NAME = "http://115.112.122.98:8888";
-//	public static final String DOMAIN_NAME = "http://192.168.0.109:8080";
+
+	//public static final String DOMAIN_NAME = "http://192.168.0.109:8080";
+
+	 public static final String DOMAIN_NAME = "http://54.235.76.180:8080";
 
 	// This is the url for getting the bars list from server
 	public static final String URL_GET_BAR_LIST = DOMAIN_NAME
 			+ "/Bartsy/venue/getMenu";
+	// This is the url for posting the Profiles Data to server
+	public static final String URL_Post_Profile_Data = "http://192.168.0.109:8080/Bartsy/user/saveUserProfile";
 }
--- src/com/kellislabs/bartsy/utils/WebServices.java
+++ src/com/kellislabs/bartsy/utils/WebServices.java
@@ -7,13 +7,17 @@ import java.net.URI;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.util.Log;
 
 import com.kellislabs.bartsy.db.DatabaseManager;
 import com.kellislabs.bartsy.model.MenuDrink;
@@ -42,6 +46,54 @@ public class WebServices {
 
 			return false;
 		}
+
+	}
+
+	/**
+	 * Create a new HttpClient and Post data
+	 * 
+	 * @param url
+	 * @param postData
+	 * @param context
+	 * @return
+	 * @throws Exception
+	 */
+	public static String postRequest(String url, JSONObject postData,
+			Context context) throws Exception {
+
+		String response = null;
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost(url);
+
+		String data = postData.toString();
+		System.out.println("*** " + data);
+		try {
+			boolean status = isNetworkAvailable(context);
+			if (status == true) {
+				try {
+					httppost.setEntity(new StringEntity(data));
+
+					// Execute HTTP Post Request
+
+					httppost.setHeader("Accept", "application/json");
+					httppost.setHeader("Content-type", "application/json");
+
+					HttpResponse httpResponse = httpclient.execute(httppost);
+
+					String responseofmain = EntityUtils.toString(httpResponse
+							.getEntity());
+					response = responseofmain.toString();
+				} catch (Exception e) {
+					Log.e("log_tag", "Error in http connection" + e.toString());
+					System.out.println("::: " + e.getMessage());
+
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		return response;
 
 	}
 
@@ -87,56 +139,71 @@ public class WebServices {
 
 		System.out.println("get menu list");
 		String response = null;
-		response = getRequest(Constants.URL_GET_BAR_LIST, context);
+		JSONObject json = new JSONObject();
+		try {
+			json.put("venueId", "100001");
+			response = postRequest(Constants.URL_GET_BAR_LIST, json, context);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 		if (response == null) {
 
 		} else {
 			try {
 				JSONArray jsonArray = new JSONArray(response);
 				System.out.println("json arrya " + jsonArray.length());
-				
+
 				for (int section = 0; section < jsonArray.length(); section++) {
 
 					JSONObject jsonObject = jsonArray.getJSONObject(section);
 					Section menuSection = null;
-					if (jsonObject.has("section_name") && jsonObject.has("subsections")) {
-						
+					if (jsonObject.has("section_name")
+							&& jsonObject.has("subsections")) {
+
 						String name = jsonObject.getString("section_name");
-						
-						JSONArray subsections = jsonObject.getJSONArray("subsections");
+
+						JSONArray subsections = jsonObject
+								.getJSONArray("subsections");
 						if (subsections != null && subsections.length() > 0) {
-							if (name.trim().length()>0 && subsections.length()==1) {
+							if (name.trim().length() > 0
+									&& subsections.length() == 1) {
 								menuSection = new Section();
-								if(name.length()>0)
-								menuSection.setName(name);
-								DatabaseManager.getInstance().saveSection(menuSection);
+								if (name.length() > 0)
+									menuSection.setName(name);
+								DatabaseManager.getInstance().saveSection(
+										menuSection);
 							}
-						
+
 							for (int i = 0; i < subsections.length(); i++) {
-									JSONObject subSection = subsections.getJSONObject(i);
-									String subName = subSection.getString("subsection_name");
-									
-									if(subName.trim().length()>0){
-										String newName = name+" - "+subName;
-										menuSection = new Section();
-										menuSection.setName(newName);
-										DatabaseManager.getInstance().saveSection(menuSection);
-									}
-									
-									JSONArray contents = subSection.getJSONArray("contents");
-									for (int k = 0; k < contents.length(); k++)
-									{
-										MenuDrink menuDrink = new MenuDrink(
-												contents.getJSONObject(k));
-										menuDrink.setSection(menuSection);
-										DatabaseManager.getInstance().saveDrink(menuDrink);
-									}
+								JSONObject subSection = subsections
+										.getJSONObject(i);
+								String subName = subSection
+										.getString("subsection_name");
+
+								if (subName.trim().length() > 0) {
+									String newName = name + " - " + subName;
+									menuSection = new Section();
+									menuSection.setName(newName);
+									DatabaseManager.getInstance().saveSection(
+											menuSection);
+								}
+
+								JSONArray contents = subSection
+										.getJSONArray("contents");
+								for (int k = 0; k < contents.length(); k++) {
+									MenuDrink menuDrink = new MenuDrink(
+											contents.getJSONObject(k));
+									menuDrink.setSection(menuSection);
+									DatabaseManager.getInstance().saveDrink(
+											menuDrink);
 								}
 							}
 						}
-						
 					}
 
+				}
 
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
