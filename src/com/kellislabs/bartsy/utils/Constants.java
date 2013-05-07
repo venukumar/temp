07f6ba7f4c8f5e15bf982187de469be5495ca0ea
srikantht <srikantht@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-07 12:46:02 +0000

    profile webservice integration
    git-svn-id: http://192.168.2.90/svn/Vendsy/Bartsy/Android/Bartsy@96 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/kellislabs/bartsy/utils/Constants.java
+++ src/com/kellislabs/bartsy/utils/Constants.java
@@ -2,14 +2,17 @@ package com.kellislabs.bartsy.utils;
 
 public class Constants {
 
+	// public static final String DOMAIN_NAME = "http://192.168.0.109:8080";
 
-	//public static final String DOMAIN_NAME = "http://192.168.0.109:8080";
-
-	 public static final String DOMAIN_NAME = "http://54.235.76.180:8080";
+	public static final String DOMAIN_NAME = "http://54.235.76.180:8080";
 
 	// This is the url for getting the bars list from server
 	public static final String URL_GET_BAR_LIST = DOMAIN_NAME
 			+ "/Bartsy/venue/getMenu";
 	// This is the url for posting the Profiles Data to server
-	public static final String URL_Post_Profile_Data = "http://192.168.0.109:8080/Bartsy/user/saveUserProfile";
+	public static final String URL_Post_Profile_Data = DOMAIN_NAME
+			+ "/Bartsy/user/saveUserProfile";
+
+	// Android Device Type
+	public static final int DEVICE_Type = 0;
 }
