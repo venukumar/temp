729dc3e36818dae989e2a6ff05da986f1f8f0ffe
malireddy <malireddy@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-05-16 09:44:55 +0000

    Removed hard coded code and used in Constants class
    git-svn-id: http://192.168.2.90/svn/Vendsy/Bartsy/Android/Bartsy@251 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/vendsy/bartsy/utils/Constants.java
+++ src/com/vendsy/bartsy/utils/Constants.java
@@ -3,32 +3,32 @@ package com.vendsy.bartsy.utils;
 
 public class Constants {
 	
-	//public static final String DOMAIN_NAME="http://54.235.76.180:8080/Bartsy_Sprint1/";
-	
-	//public static final String DOMAIN_NAME = "http://192.168.0.109:8080/Bartsy/";
+	//public static final String DOMAIN_NAME = "http://192.168.0.109:8080/";
 
-	public static final String DOMAIN_NAME = "http://54.235.76.180:8080/Bartsy/";
+	public static final String DOMAIN_NAME = "http://54.235.76.180:8080/";
+	
+	public static final String PROJECT_NAME ="Bartsy/";
 
 	public static final boolean USE_ALLJOYN = false;
 
 	// This is the url for getting the bars list from server
-	public static final String URL_GET_BAR_LIST = DOMAIN_NAME
+	public static final String URL_GET_BAR_LIST = DOMAIN_NAME + PROJECT_NAME
 			+ "venue/getMenu";
 	// This is the url for posting the Profiles Data to server
-	public static final String URL_POST_PROFILE_DATA = DOMAIN_NAME
+	public static final String URL_POST_PROFILE_DATA = DOMAIN_NAME + PROJECT_NAME
 			+ "user/saveUserProfile";
 	// This is the url for place the order
-	public static final String URL_PLACE_ORDER = DOMAIN_NAME
+	public static final String URL_PLACE_ORDER = DOMAIN_NAME + PROJECT_NAME
 			+ "order/placeOrder";
 	// This is the url for getting the venu list from server
-	public static final String URL_GET_VENU_LIST = DOMAIN_NAME
+	public static final String URL_GET_VENU_LIST = DOMAIN_NAME + PROJECT_NAME
 			+ "venue/getVenueList";
 	// This is the url for User Check In
-	public static final String URL_USER_CHECK_IN = DOMAIN_NAME
+	public static final String URL_USER_CHECK_IN = DOMAIN_NAME + PROJECT_NAME
 			+ "user/userCheckIn";
 
 	// This is the url for User Check Out
-	public static final String URL_USER_CHECK_OUT = DOMAIN_NAME
+	public static final String URL_USER_CHECK_OUT = DOMAIN_NAME + PROJECT_NAME
 			+ "user/userCheckOut";
 
 	// Android Device Type
