4439a5815ea0d4e5723f02f4fbb51c159cbadff5
malireddy <malireddy@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-09 09:17:58 +0000

    For Pagination list view, It is require to know when the user scroll to end of the screen
    git-svn-id: http://192.168.2.90/svn/Vendsy/Bartsy/Android/Bartsy@922 c3b44e55-a100-42f6-9c86-32dbf579054b

---
+++ src/com/vendsy/bartsy/ResponsiveScrollView.java
@@ -0,0 +1,57 @@
+package com.vendsy.bartsy;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+public class ResponsiveScrollView extends ScrollView {
+
+    public interface OnEndScrollListener {
+        public void onEndScroll();
+    }
+
+    private boolean mIsFling;
+    private OnEndScrollListener mOnEndScrollListener;
+
+    public ResponsiveScrollView(Context context) {
+        this(context, null, 0);
+        
+        
+    }
+
+    public ResponsiveScrollView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ResponsiveScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public void fling(int velocityY) {
+        super.fling(velocityY);
+        mIsFling = true;
+    }
+
+    @Override
+    protected void onScrollChanged(int x, int y, int oldX, int oldY) {
+        super.onScrollChanged(x, y, oldX, oldY);
+        if (mIsFling) {
+            if (Math.abs(y - oldY) < 2 || y >= getMeasuredHeight() || y == 0) {
+                if (mOnEndScrollListener != null) {
+                    mOnEndScrollListener.onEndScroll();
+                }
+                mIsFling = false;
+            }
+        }
+    }
+
+    public OnEndScrollListener getOnEndScrollListener() {
+        return mOnEndScrollListener;
+    }
+
+    public void setOnEndScrollListener(OnEndScrollListener mOnEndScrollListener) {
+        this.mOnEndScrollListener = mOnEndScrollListener;
+    }
+
+}
