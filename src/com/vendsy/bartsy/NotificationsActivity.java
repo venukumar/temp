b00b28cc2e688e93d42155d3c88ad180b2d99512
malireddy <malireddy@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-09 09:16:12 +0000

    Removed the offer drink type and added scrollview
    git-svn-id: http://192.168.2.90/svn/Vendsy/Bartsy/Android/Bartsy@921 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/vendsy/bartsy/NotificationsActivity.java
+++ src/com/vendsy/bartsy/NotificationsActivity.java
@@ -11,11 +11,15 @@ import android.app.ProgressDialog;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.LinearLayout.LayoutParams;
 
 import com.vendsy.bartsy.model.Notification;
 import com.vendsy.bartsy.utils.Utilities;
@@ -43,7 +47,23 @@ public class NotificationsActivity extends Activity{
 		
 		mApp = (BartsyApplication) getApplication();
 		
-		notificationsListView = (LinearLayout)findViewById(R.id.people_notifications);
+		LinearLayout notificationsLayout = (LinearLayout)findViewById(R.id.people_notifications);
+		
+		ScrollView scrollView = new ScrollView(this);
+		notificationsLayout.addView(scrollView);
+		
+		notificationsListView = new LinearLayout(this);
+		// Set parameters for linear layout
+		LayoutParams params = new LayoutParams(android.view.ViewGroup.LayoutParams.MATCH_PARENT, android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
+		notificationsListView.setLayoutParams(params);
+		notificationsListView.setOrientation(LinearLayout.VERTICAL);
+				
+		// Set dividers
+		notificationsListView.setDividerDrawable(getResources().getDrawable(R.drawable.div_light_grey));
+		notificationsListView.setShowDividers(LinearLayout.SHOW_DIVIDER_MIDDLE | LinearLayout.SHOW_DIVIDER_BEGINNING | LinearLayout.SHOW_DIVIDER_END);
+		
+		scrollView.addView(notificationsListView);
+//		notificationsListView.
 		
 		loadNotifications();
 	}
@@ -76,8 +96,6 @@ public class NotificationsActivity extends Activity{
 						}
 					}
 				});
-				
-
 			};
 		}.start();
 
@@ -122,7 +140,8 @@ public class NotificationsActivity extends Activity{
 		
 		for(Notification notification: notifications){
 			// Add offer drink view and set the notification data 
-			if(Notification.TYPE_OFFER_DRINK.equals(notification.getType())){
+//			if(notification.getOrderType() != null && (notification.getOrderType().equals("offer") || notification.getOrderType().equals("placeOrder"))){
+			if(Notification.TYPE_PLACE_ORDER.equals(notification.getType())){
 				addOfferDrinkView(notification);
 			}
 			// Add checkin or checkout view and set the notification data 
@@ -131,7 +150,7 @@ public class NotificationsActivity extends Activity{
 			}
 		}
 	}
-	
+
 	/**
 	 * Add different type notification(checkin/checkout/place order/order update) view to the main listview and set the notification values
 	 * 
@@ -141,16 +160,24 @@ public class NotificationsActivity extends Activity{
 		// Inflate checkin view
 		View view = getLayoutInflater().inflate(R.layout.notifications_item, null);
 		
+		
 		// Set all notification information to the view
-		
-		((ImageView)view.findViewById(R.id.profileImage)).setImageBitmap(mApp.mProfile.getImage());
+		ImageView profileImage = ((ImageView)view.findViewById(R.id.profileImage));
 		((TextView)view.findViewById(R.id.messageText)).setText(notification.getMessage());
-		((TextView)view.findViewById(R.id.dateText)).setText(notification.getCreatedTime());
 		
+		((TextView)view.findViewById(R.id.dateText)).setText(Utilities.getFriendlyDate(notification.getCreatedTime(), "d MMM yyyy HH:mm:ss 'GMT'"));
+		
+		// Set profile image based on the notification type
+		((TextView)view.findViewById(R.id.userNameText)).setText(notification.getVenueName());
+		profileImage.setTag(WebServices.DOMAIN_NAME+notification.getVenueImage());
+		WebServices.downloadImage(profileImage, mApp);		
+		
+		ImageView imageView = ((ImageView)view.findViewById(R.id.messageTypeImage));
 		// If the notification is related to checkout then replace messagetype image with checkout image 
 		if(Notification.TYPE_CHECKOUT.equals(notification.getType())){
-			ImageView imageView = ((ImageView)view.findViewById(R.id.messageTypeImage));
 			imageView.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.checkout));
+		}else if(Notification.TYPE_UPDATE_ORDER.equals(notification.getType())){
+			imageView.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.sambuca));
 		}
 		
 		// Add view to the notification list view
@@ -169,7 +196,27 @@ public class NotificationsActivity extends Activity{
 		// Set all notification information to the view
 		((ImageView)view.findViewById(R.id.profileImage)).setImageBitmap(mApp.mProfile.getImage());
 		((TextView)view.findViewById(R.id.messageText)).setText(notification.getMessage());
-		((TextView)view.findViewById(R.id.dateText)).setText(notification.getCreatedTime());
+		
+		((TextView)view.findViewById(R.id.dateText)).setText(Utilities.getFriendlyDate(notification.getCreatedTime(), "d MMM yyyy HH:mm:ss 'GMT'"));
+		
+		ImageView otherProfileImage = ((ImageView)view.findViewById(R.id.otherProfileImage));
+		
+		if(Notification.TYPE_PLACE_ORDER.equals(notification.getType())){
+			ImageView imageView = ((ImageView)view.findViewById(R.id.messageTypeImage));
+			imageView.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.sambuca));
+			
+			// Set venue profile image
+			((TextView)view.findViewById(R.id.userNameText)).setText(notification.getVenueName());
+			if(notification.hasVenueImage()){
+				otherProfileImage.setTag(WebServices.DOMAIN_NAME+notification.getVenueImage());
+			}
+			
+		}else if(notification.getOrder()!=null){
+			// Set other person profile image
+			((TextView)view.findViewById(R.id.userNameText)).setText(notification.getOrder().recipientNickname);
+			otherProfileImage.setTag(WebServices.DOMAIN_NAME+notification.getOrder().recipientImagePath);
+		}
+		WebServices.downloadImage(otherProfileImage, mApp);
 		
 		// Add view to the notification list view
 		notificationsListView.addView(view);
