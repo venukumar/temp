b9b0f6e5c129b94b40b65b1a1a97ffd625ea2824
peter.k <peter.k@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-07-14 11:49:17 +0000

    Fixed null pointer exception when displaying closed venues and enhanced the venues list UI
    git-svn-id: http://192.168.2.90/svn/Vendsy/Bartsy/Android/Bartsy@1010 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/vendsy/bartsy/adapter/VenueListViewAdapter.java
+++ src/com/vendsy/bartsy/adapter/VenueListViewAdapter.java
@@ -6,6 +6,7 @@ import java.text.DecimalFormat;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationManager;
 import android.view.LayoutInflater;
@@ -45,19 +46,27 @@ public class VenueListViewAdapter extends ArrayAdapter<Venue> {
 
 		// Setup view if needed
 		LayoutInflater vi = LayoutInflater.from(getContext());
+		view = vi.inflate(R.layout.map_list_item, null);
 			
 		// Show people present if venue is open
 		if (!venue.getStatus().equalsIgnoreCase("CLOSED")) {
-			view = vi.inflate(R.layout.map_list_item, null);
+
+			view.findViewById(R.id.view_map_checkins).setVisibility(View.VISIBLE);
+			view.findViewById(R.id.view_map_closed).setVisibility(View.GONE);
+			
 			((TextView) view.findViewById(R.id.view_map_venue_people_count)).setText(Integer.toString(venue.getUserCount()));
 			if (venue.getUserCount() == 1) 
 				((TextView) view.findViewById(R.id.view_map_people_text)).setText(" person checked in");
 			else
 				((TextView) view.findViewById(R.id.view_map_people_text)).setText(" people checked in");
 		} else {
-			view = vi.inflate(R.layout.map_list_item_closed, null);
+			view.findViewById(R.id.view_map_checkins).setVisibility(View.GONE);
+			view.findViewById(R.id.view_map_closed).setVisibility(View.VISIBLE);
+			
+			((TextView) view.findViewById(R.id.view_map_distance)).setTextColor(parent.getResources().getColor(android.R.color.darker_gray));
+			((TextView) view.findViewById(R.id.view_map_venue_name)).setTextColor(parent.getResources().getColor(android.R.color.darker_gray));
 		}
-
+	
 		// Display basic venue details
 		((TextView) view.findViewById(R.id.view_map_venue_name)).setText(venue.getName());
 		((TextView) view.findViewById(R.id.view_map_venue_address)).setText(venue.getAddress());
@@ -78,13 +87,17 @@ public class VenueListViewAdapter extends ArrayAdapter<Venue> {
 	
 			// Format distance
 			DecimalFormat df = new DecimalFormat();
-			df.setMaximumFractionDigits(2);
-			df.setMinimumFractionDigits(2);
 	
 			// Display distance
-			if(distance==-1){
+			if (distance==-1) {
 				((TextView) view.findViewById(R.id.view_map_distance)).setText("-");
-			}else{
+			} else if (distance < 10 ){
+				df.setMaximumFractionDigits(2);
+				df.setMinimumFractionDigits(2);
+				((TextView) view.findViewById(R.id.view_map_distance)).setText(df.format(distance));
+			} else {
+				df.setMaximumFractionDigits(0);
+				df.setMinimumFractionDigits(0);
 				((TextView) view.findViewById(R.id.view_map_distance)).setText(df.format(distance));
 			}
 		} else {
@@ -94,8 +107,10 @@ public class VenueListViewAdapter extends ArrayAdapter<Venue> {
 		// If we're checked in display check-mark
 		if (activeVenue != null && activeVenue.getId().equals(venue.getId())) {
 			view.findViewById(R.id.view_map_venue_checked_in).setVisibility(View.VISIBLE);
+			view.findViewById(R.id.view_map_distance_option).setVisibility(View.GONE);
 		} else {
 			view.findViewById(R.id.view_map_venue_checked_in).setVisibility(View.GONE);
+			view.findViewById(R.id.view_map_distance_option).setVisibility(View.VISIBLE);
 		}
 		
 		return view;
