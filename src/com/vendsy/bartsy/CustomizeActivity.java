b6b1db9e0616acc56622aeeeafc196e9ad6e22d3
spoorthy <spoorthy@c3b44e55-a100-42f6-9c86-32dbf579054b>
2013-08-29 07:51:27 +0000

    1.Added action buttons favourite, likeand share buttons. 2.On liking an item posting the item feed on Facebook . 3.Share the item with other apps present in the user's mobile
    git-svn-id: http://192.168.2.90/svn/Vendsy/Bartsy/Android/Bartsy@1589 c3b44e55-a100-42f6-9c86-32dbf579054b

--- src/com/vendsy/bartsy/CustomizeActivity.java
+++ src/com/vendsy/bartsy/CustomizeActivity.java
@@ -1,26 +1,51 @@
 package com.vendsy.bartsy;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.LayoutParams;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
+import com.facebook.FacebookAuthorizationException;
+import com.facebook.FacebookOperationCanceledException;
+import com.facebook.FacebookRequestError;
+import com.facebook.HttpMethod;
+import com.facebook.Request;
+import com.facebook.RequestAsyncTask;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphObject;
+import com.facebook.model.GraphUser;
 import com.vendsy.bartsy.dialog.DrinkDialogFragment;
 import com.vendsy.bartsy.model.Item;
 import com.vendsy.bartsy.model.UserProfile;
 import com.vendsy.bartsy.utils.Utilities;
 import com.vendsy.bartsy.utils.WebServices;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,12 +62,29 @@ public class CustomizeActivity extends SherlockActivity implements OnClickListen
 	private BartsyApplication mApp;
 	private Item mItem;
 	private Handler mHandler = new Handler();
+	private static final String PENDING_PUBLISH_KEY = "pendingPublishReauthorization";
+	private boolean pendingPublishReauthorization = false;
+	private UiLifecycleHelper uiHelper;
+	private ImageView like;
 	
+	// Callback for Facebook session status
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+		@Override
+		public void call(Session session, SessionState state,
+				Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
+	};
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		uiHelper = new UiLifecycleHelper(this, callback);
+        uiHelper.onCreate(savedInstanceState);
 		
+		if (savedInstanceState != null) {
+			pendingPublishReauthorization = savedInstanceState.getBoolean(
+					PENDING_PUBLISH_KEY, false);
+		}
 		mApp = (BartsyApplication) getApplication();
 		
 		// Make sure the input is still valid (in case we're lost the application object when this activity started)
@@ -60,9 +102,54 @@ public class CustomizeActivity extends SherlockActivity implements OnClickListen
 		final ActionBar actionBar = getSupportActionBar();
 		actionBar.setDisplayShowHomeEnabled(true);
 		actionBar.setDisplayHomeAsUpEnabled(true);
-		if (mItem.hasTitle()) actionBar.setTitle(mItem.getName());
 		actionBar.setIcon(R.drawable.circle_pink);
+
+		// Inflate the view and set as custom view for action bar
+		LayoutInflater inflator = (LayoutInflater) this
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View customView = inflator.inflate(R.layout.header_order_customize,
+				null);
+		actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP
+				| ActionBar.DISPLAY_SHOW_CUSTOM | ActionBar.DISPLAY_SHOW_HOME
+				| ActionBar.DISPLAY_SHOW_TITLE | ActionBar.DISPLAY_USE_LOGO);
+		LayoutParams lp = new LayoutParams(LayoutParams.WRAP_CONTENT,
+				LayoutParams.WRAP_CONTENT, Gravity.RIGHT
+						| Gravity.CENTER_VERTICAL);
+		actionBar.setCustomView(customView, lp);
+		//Set the title
+		if (mItem.hasTitle())
+			actionBar.setTitle(mItem.getName());
 		
+		// Share the item 
+		ImageView shareButton=(ImageView)customView.findViewById(R.id.view_order_share);
+		shareButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_SEND);
+			    intent.setType("text/plain");
+			    intent.putExtra(Intent.EXTRA_SUBJECT,"Liked "+mItem.getName()+" on "+mApp.mActiveVenue.getName()+" with Bartsy");
+			    intent.putExtra(Intent.EXTRA_TEXT, "Download Bartsy Application from Google Play for more details");  
+			    startActivity(Intent.createChooser(intent, "Share with"));
+				
+			}
+		});
+		like = (ImageView) customView.findViewById(R.id.view_order_like);
+		like.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				likeAlert("Are you sure you want to like " + mItem.getName()
+						+ " on Facebook");
+			}
+		});
+		
+		// Set the visibility of the like button to true only if the user logins through Facebook
+		if (mApp.mProfile.hasFacebookId()) {
+			like.setVisibility(View.VISIBLE);
+		} else {
+			like.setVisibility(View.GONE);
+		}
 		
 		// Set the main view
 		setContentView(mItem.customizeView(getLayoutInflater()));
@@ -70,10 +157,8 @@ public class CustomizeActivity extends SherlockActivity implements OnClickListen
 		// Set up the favorites checkbox
 		if (mItem.has(mItem.getFavoriteId())) {
 			((CheckBox) findViewById(R.id.view_order_item_favorite)).setChecked(true);
-			((CheckBox) findViewById(R.id.view_order_item_favorite)).setText("Saved to favorites");
 		} else {
 			((CheckBox) findViewById(R.id.view_order_item_favorite)).setChecked(false);
-			((CheckBox) findViewById(R.id.view_order_item_favorite)).setText("Save to favorites");
 		}
 		
 		// Set up listeners
@@ -82,6 +167,148 @@ public class CustomizeActivity extends SherlockActivity implements OnClickListen
 		findViewById(R.id.view_order_item_favorite).setOnClickListener(this);
 	}
 	
+	/**
+	 * Display confirmation alert box when the user selects like button
+	 * 
+	 * @param message
+	 */
+	private void likeAlert(String message) {
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(
+				CustomizeActivity.this);
+		builder.setCancelable(true);
+		builder.setTitle("Like us");
+		builder.setInverseBackgroundForced(true);
+		builder.setMessage(message);
+		builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+
+				dialog.dismiss();
+				like.setImageResource(R.drawable.like_blue);
+				publishItem();
+
+			}
+		});
+		builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		});
+		AlertDialog alert = builder.create();
+		alert.show();
+
+	}
+
+	/**
+	 * Publish the feed on Facebook wall
+	 */
+	protected void publishItem() {
+		Session session = Session.getActiveSession();
+		if (session != null) {
+			if (session.isOpened()) {
+				// Check for publish permissions
+				if (pendingPublishReauthorization == false) {
+					postParams(session);
+				}
+
+			} else {
+				// Open the session if the session is closed or inactive for publishing the feed
+				Session.openActiveSession(this, true,
+						new Session.StatusCallback() {
+
+							@Override
+							public void call(final Session session,
+									SessionState state, Exception exception) {
+
+								if (session.isOpened()) {
+									Request.executeMeRequestAsync(session,
+											new Request.GraphUserCallback() {
+												@Override
+												public void onCompleted(
+														GraphUser user,
+														Response response) {
+													if (user != null
+															&& pendingPublishReauthorization == false) {
+														postParams(session);
+													}
+												}
+											});
+								} else {
+									Log.d("SESSION NOT OPENED",
+											"SESSION NOT OPENED");
+								}
+							}
+						});
+			}
+		}
+
+	}
+	
+	/**
+	 * Post the params to be published on the Facebook feed
+	 * @param session
+	 */
+	private void postParams(Session session) {
+		Bundle postParams = new Bundle();
+		postParams.putString("name", "Bartsy");
+		postParams.putString("caption", "Liked " + mItem.getName() + " on"
+				+ mApp.mActiveVenue.getName());
+		postParams.putString("description", mItem.getDescription());
+		postParams.putString("link", "");
+		postParams.putString("picture", mApp.mActiveVenue.getImagePath());
+		
+		Request.Callback callback = new Request.Callback() {
+			public void onCompleted(Response response) {
+				if (response != null) {
+					Log.i(TAG, response.toString());
+					JSONObject graphResponse = response.getGraphObject()
+							.getInnerJSONObject();
+					String postId = null;
+					try {
+						// PostId obtained after publishing the feed
+						postId = graphResponse.getString("id");
+					} catch (JSONException e) {
+						Log.i(TAG, "JSON error " + e.getMessage());
+					}
+					FacebookRequestError error = response.getError();
+					try{
+					if (error != null) {
+						Toast.makeText(CustomizeActivity.this,
+								error.getErrorMessage(), Toast.LENGTH_SHORT)
+								.show();
+					} else {
+						Toast.makeText(CustomizeActivity.this,
+
+						postId, Toast.LENGTH_LONG).show();
+					}
+					}catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+
+		Request request = new Request(session, "me/feed", postParams,
+				HttpMethod.POST, callback);
+
+		RequestAsyncTask task = new RequestAsyncTask(request);
+		task.execute();
+
+	}
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+		 if (pendingPublishReauthorization && 
+			        state.equals(SessionState.OPENED_TOKEN_UPDATED)) {
+			    pendingPublishReauthorization = false;
+			    publishItem();
+			}
+	 }
+
+
+
+
+
 	/** 
 	 * TODO - Activity input/output 
 	 */
@@ -188,11 +415,6 @@ public class CustomizeActivity extends SherlockActivity implements OnClickListen
 
 	private void addOrRemovefavorite(final CheckBox favoriteCheckBox) {
 		
-		String message = "Deleting favorite...";
-		if(favoriteCheckBox.isChecked()){
-			message = "Saving to favorites...";
-		}
-		favoriteCheckBox.setText(message);
 		new Thread(){
 			public void run() {
 				// Add to favorites
@@ -233,11 +455,9 @@ public class CustomizeActivity extends SherlockActivity implements OnClickListen
 
 					@Override public void run() {
 						if (checked) {
-							favoriteCheckBox.setText("Saved to favorites");
 							favoriteCheckBox.setChecked(true);
 							Toast.makeText(CustomizeActivity.this, "Saved to favorites", Toast.LENGTH_SHORT).show();
 						} else {
-							favoriteCheckBox.setText("Save to favorites");
 							favoriteCheckBox.setChecked(false);
 							mApp.makeText("Removed from favorites", Toast.LENGTH_SHORT);
 						}
